3b3974caeb0623d05c1875dbb1f65fc5dfa719ff
kimapu <kimlee@staffemail.apu.edu.my>
2021-11-16 17:56:15 +0800

    display record in jtable

---
+++ myoodj-alt/src/faq/fileIO/WindowRecordShow.java
@@ -0,0 +1,81 @@
+package faq.fileIO;
+
+import java.awt.EventQueue;
+import java.util.List;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+
+public class WindowRecordShow extends JFrame {
+
+	User u = new User();
+	
+	private JPanel contentPane;
+	private JTable table;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					WindowRecordShow frame = new WindowRecordShow();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public WindowRecordShow() {
+		setTitle("SHOW RECORD");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 526, 350);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(10, 11, 490, 216);
+		contentPane.add(scrollPane);
+		
+		table = new JTable();
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+			},
+			new String[] {
+				"FIRSTNAME", "LASTNAME"
+			}
+		) {
+			Class[] columnTypes = new Class[] {
+				String.class, String.class
+			};
+			public Class getColumnClass(int columnIndex) {
+				return columnTypes[columnIndex];
+			}
+		});
+		scrollPane.setViewportView(table);
+		updateTableModel();
+	}
+	
+	private void updateTableModel(){
+		DefaultTableModel model = (DefaultTableModel)table.getModel();
+		List<User> lst = u.getAll();
+		for (User user : lst) {
+			String[] record = { user.getFirst(), user.getLast() };
+			model.addRow( record );
+		}
+		
+	}
+
+}
